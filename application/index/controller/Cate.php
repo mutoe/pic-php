24665c6cbb00c21e1e6756a66e8ca2ae97d72957
mutoe <mutoe@foxmail.com>
2017-01-17 17:58:10 +0800

    优化 分区页 分页

--- application/index/controller/Cate.php
+++ application/index/controller/Cate.php
@@ -9,7 +9,7 @@ class Cate extends Controller {
         $share = model('Share');
         $cate  = model('Cate');
 
-        $data = $share->where(['cate_id' => $cate_id, 'status' => ['gt', 0]])->select();
+        $data = $share->where(['cate_id' => $cate_id, 'status' => ['gt', 0]])->paginate(24);
 
         $cate_data = $cate->where('cate_id', $cate_id)->find();
 
@@ -19,7 +19,7 @@ class Cate extends Controller {
         $cate_click_list = \think\Cache::get('cate_click_list');
 
         return $this->fetch('detail', [
-            'cate_data' => $data,
+            'list' => $data,
             'cate_info' => $cate_data,
             'cate_click' => $cate_click_list[$cate_id],
         ]);
