a652b9137663871a0a061968f83c56a64b6d15a5
mutoe <mutoe@foxmail.com>
2017-03-26 22:20:30 +0800

    修复 修正页脚位置始终位于页面或窗口底部

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,14 +1,17 @@
-(function($) {
-  'use strict';
 
-  $(function() {
-    var $fullText = $('.admin-fullText');
-    $('#admin-fullscreen').on('click', function() {
-      $.AMUI.fullscreen.toggle();
-    });
+// jQuery.resize.min.js
+(function($,h,c){var a=$([]),e=$.resize=$.extend($.resize,{}),i,k="setTimeout",j="resize",d=j+"-special-event",b="delay",f="throttleWindow";e[b]=250;e[f]=true;$.event.special[j]={setup:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.add(l);$.data(this,d,{w:l.width(),h:l.height()});if(a.length===1){g()}},teardown:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.not(l);l.removeData(d);if(!a.length){clearTimeout(i)}},add:function(l){if(!e[f]&&this[k]){return false}var n;function m(s,o,p){var q=$(this),r=$.data(this,d);r.w=o!==c?o:q.width();r.h=p!==c?p:q.height();n.apply(this,arguments)}if($.isFunction(l)){n=l;return m}else{n=l.handler;l.handler=m}}};function g(){i=h[k](function(){a.each(function(){var n=$(this),m=n.width(),l=n.height(),o=$.data(this,d);if(m!==o.w||l!==o.h){n.trigger(j,[o.w=m,o.h=l])}});g()},e[b])}})(jQuery,this);
 
-    $(document).on($.AMUI.fullscreen.raw.fullscreenchange, function() {
-      $fullText.text($.AMUI.fullscreen.isFullscreen ? '退出全屏' : '开启全屏');
-    });
-  });
-})(jQuery);
+/**
+ * 页脚位置修正
+ * 调用此函数后将页脚重新放置在页面底部
+ */
+function footer_replace() {
+    var $prev = $('.footer').prev();
+    var realHeight = $prev.offset().top + $prev.height() + $('.footer').height();
+    if (realHeight < $(window).height()) {
+        $('.footer').css('position', 'fixed');
+    } else {
+        $('.footer').css('position', 'relative');
+    }
+}
