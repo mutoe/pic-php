41e9cdee188e980fc5bb705373fe9296bcc1c957
mutoe <mutoe@foxmail.com>
2017-03-26 23:03:07 +0800

    优化 去除冗余代码 代码格式化

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,9 +1,20 @@
 
-// jQuery.resize.min.js
-(function($,h,c){var a=$([]),e=$.resize=$.extend($.resize,{}),i,k="setTimeout",j="resize",d=j+"-special-event",b="delay",f="throttleWindow";e[b]=250;e[f]=true;$.event.special[j]={setup:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.add(l);$.data(this,d,{w:l.width(),h:l.height()});if(a.length===1){g()}},teardown:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.not(l);l.removeData(d);if(!a.length){clearTimeout(i)}},add:function(l){if(!e[f]&&this[k]){return false}var n;function m(s,o,p){var q=$(this),r=$.data(this,d);r.w=o!==c?o:q.width();r.h=p!==c?p:q.height();n.apply(this,arguments)}if($.isFunction(l)){n=l;return m}else{n=l.handler;l.handler=m}}};function g(){i=h[k](function(){a.each(function(){var n=$(this),m=n.width(),l=n.height(),o=$.data(this,d);if(m!==o.w||l!==o.h){n.trigger(j,[o.w=m,o.h=l])}});g()},e[b])}})(jQuery,this);
+/**
+ * 弹出消息窗口
+ * @author 杨栋森 mutoe@foxmail.com at 2017-03-26
+ *
+ * @param  {String} msg 弹出消息内容
+ */
+function amalert(msg) {
+    $alert = $('#am-alert');
+    $alert.find('.content').html(msg);
+    $alert.modal('open');
+}
 
 /**
  * 页脚位置修正
+ * @author 杨栋森 mutoe@foxmail.com at 2017-03-26
+ *
  * 调用此函数后将页脚重新放置在页面底部
  */
 function footer_replace() {
@@ -15,3 +26,17 @@ function footer_replace() {
         $('.footer').css('position', 'relative');
     }
 }
+
+/**
+ * 刷新验证码
+ * @author 杨栋森 mutoe@foxmail.com at 2016-07-20
+ *
+ * 需要将验证码 以下面的方式放置
+ * <div id="captcha-img"><img src="{:captcha_src()}" alt="captcha" /></div>
+ * 点击div#captcha-img后会重新拉取验证码
+ */
+$('#captcha-img').on('click', function() {
+    $.get('index/captcha', function(data) {
+        $('#captcha-img img').attr('src', "{:captcha_src()}?t="+ new Date().getTime())
+    });
+});
