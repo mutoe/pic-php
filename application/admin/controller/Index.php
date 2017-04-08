d1b96a37c00607c71c086b06cf183137453436ab
mutoe <mutoe@foxmail.com>
2017-04-09 00:24:31 +0800

    优化 未登录时跳转到登录页面

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,10 +1,27 @@
 <?php
 namespace app\admin\controller;
 
-class Index
+use \think\Controller;
+
+class Index extends Controller
 {
+
+    protected $user_id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->user_id = auth_status('user_id');
+
+        if (!$this->user_id) {
+            $this->redirect('/auth');
+        }
+
+    }
+
     public function index()
     {
         return view();
     }
+
 }
